6887687c2ab2d08fe7d08fced285d976536e9360
Alexandre Vernet <alexandre.vernet@g-mail.fr>
2022-02-25 15:59:37 +0100

    Feat - Create route '/file'

--- api/server.js
+++ api/server.js
@@ -3,7 +3,6 @@ const app = express();
 const port = process.env.PORT || 3333;
 const admin = require("firebase-admin");
 const { getFirestore } = require('firebase-admin/firestore');
-const db = getFirestore();
 require('dotenv').config();
 
 app.use(express.static('public'));
@@ -17,11 +16,27 @@ admin.initializeApp({
     })
 });
 
+const db = getFirestore();
+
 
 app.get('/', async (req, res) => {
     res.send('Hello World!');
 });
 
+app.post('/file', async (req, res) => {
+    const { uid, file } = req.body;
+
+    // Add a new document with a generated id.
+    await db.collection('files').doc(uid).set({ file }).then(() => {
+        res.status(201).send({
+            message: 'File uploaded successfully'
+        })
+    }).catch(error => {
+        res.status(500).send({
+            message: error.message
+        });
+    });
+})
 
 app.listen(port, () => {
     console.log(`App listening on port ${ port } !`);
--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -5,22 +5,25 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    ServiceWorkerModule.register('ngsw-worker.js', {
-      enabled: environment.production,
-      // Register the ServiceWorker as soon as the app is stable
-      // or after 30 seconds (whichever comes first).
-      registrationStrategy: 'registerWhenStable:30000'
-    })
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        ServiceWorkerModule.register('ngsw-worker.js', {
+            enabled: environment.production,
+            // Register the ServiceWorker as soon as the app is stable
+            // or after 30 seconds (whichever comes first).
+            registrationStrategy: 'registerWhenStable:30000'
+        })
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
