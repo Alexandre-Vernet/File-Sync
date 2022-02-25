bbd5f4151f8c2ae340ec5177a8c62425f45769b7
Alexandre Vernet <alexandre.vernet@g-mail.fr>
2022-02-25 16:13:39 +0100

    Feat - file CRUD

--- api/server.js
+++ api/server.js
@@ -23,6 +23,7 @@ app.get('/', async (req, res) => {
     res.send('Hello World!');
 });
 
+// Create
 app.post('/file', async (req, res) => {
     const { uid, file } = req.body;
 
@@ -37,6 +38,64 @@ app.post('/file', async (req, res) => {
         });
     });
 })
+
+// Read
+app.get('/file/:uid', async (req, res) => {
+    const { uid } = req.params;
+    const cityRef = db.collection('files').doc(uid);
+    const doc = await cityRef.get();
+    if (!doc.exists) {
+        res.status(404).send({
+            message: 'File not found'
+        });
+    } else {
+        res.send(doc.data());
+    }
+});
+
+// Update
+app.put('/file/:uid', async (req, res) => {
+    const { uid } = req.params;
+    const { file } = req.body;
+
+    const cityRef = db.collection('files').doc(uid);
+    await cityRef.update({ file }).then(() => {
+        res.status(200).send({
+            message: 'File updated successfully'
+        })
+    }).catch(error => {
+        res.status(500).send({
+            message: error.message
+        });
+    });
+});
+
+// Delete
+app.delete('/file/:uid', async (req, res) => {
+    const { uid } = req.params;
+
+    const cityRef = db.collection('files').doc(uid);
+    await cityRef.delete().then(() => {
+        res.status(200).send({
+            message: 'File deleted successfully'
+        })
+    }).catch(error => {
+        res.status(500).send({
+            message: error.message
+        });
+    });
+});
+
+// Find all
+app.get('/files', async (req, res) => {
+    const cityRef = db.collection('files').doc('SF');
+    const doc = await cityRef.get();
+    if (!doc.exists) {
+        res.status(404).send('No such document!');
+    } else {
+        res.send(doc.data());
+    }
+});
 
 app.listen(port, () => {
     console.log(`App listening on port ${ port } !`);
