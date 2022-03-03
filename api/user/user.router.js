f63272ddd8fd89508d172dcc3cc15654370bd6ba
Alexandre Vernet <alexandre.vernet@g-mail.fr>
2022-03-03 14:06:26 +0100

    Feat - Finalize crud user

--- api/user/user.router.js
+++ api/user/user.router.js
@@ -22,4 +22,80 @@ user.post('/', async (req, res) => {
     });
 });
 
+// Read
+user.get('/:uid', async (req, res) => {
+    const { uid } = req.params;
+
+    await db.collection('users').doc(uid).get().then(doc => {
+        if (doc.exists) {
+            res.status(200).send({
+                message: doc.data()
+            });
+        } else {
+            res.status(404).send({
+                message: 'User not found'
+            });
+        }
+    }).catch(error => {
+        res.status(500).send({
+            message: error.message
+        });
+    });
+});
+
+// Update
+user.put('/:uid', async (req, res) => {
+    const { uid } = req.params;
+    const { firstName, lastName, email, profilePicture, dateCreation } = req.body;
+
+    await db.collection('users').doc(uid).update({
+        firstName, lastName, email, profilePicture, dateCreation
+    }).then(() => {
+        res.status(200).send({
+            message: 'User has been successfully updated'
+        })
+    }).catch(error => {
+        res.status(500).send({
+            message: error.message
+        });
+    });
+});
+
+// Delete
+user.delete('/:uid', async (req, res) => {
+    const { uid } = req.params;
+
+    await db.collection('users').doc(uid).delete().then(() => {
+        res.status(200).send({
+            message: 'User has been successfully deleted'
+        })
+    }).catch(error => {
+        res.status(500).send({
+            message: error.message
+        });
+    });
+});
+
+// Find all
+user.get('/', async (req, res) => {
+    await db.collection('users').get().then(snapshot => {
+        const users = [];
+
+        snapshot.forEach(doc => {
+            users.push({
+                uid: doc.id,
+                ...doc.data()
+            });
+        });
+
+        res.status(200).send({
+            message: users
+        });
+    }).catch(error => {
+        res.status(500).send({
+            message: error.message
+        });
+    });
+});
+
 module.exports = user;
