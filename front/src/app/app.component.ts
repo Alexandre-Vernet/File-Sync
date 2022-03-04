599b580b242c33343ea6c3fdeae46b8c42c9c27a
Alexandre Vernet <alexandre.vernet@g-mail.fr>
2022-03-04 17:00:03 +0100

    Clean - Remove unused function

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,27 +1,12 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
 
-  constructor(
-      private http: HttpClient
-  ) {
-  }
-
-  sendMessage() {
-    const message = 'test';
-    this.http.post('/api/message', { message }).subscribe(
-        (data) => {
-          console.log(data);
-        },
-        (error) => {
-          console.log(error);
-        }
-    );
-  }
+    constructor() {
+    }
 }
