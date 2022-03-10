468fd0fac1bd1b2ad443277f53af2b9ba21c0b48
Alexandre Vernet <alexandre.vernet@g-mail.fr>
2022-03-10 09:45:34 +0100

    Feat - Create lists-files

---
+++ front/src/app/components/files/list-files/list-files.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-files',
+  templateUrl: './list-files.component.html',
+  styleUrls: ['./list-files.component.scss']
+})
+export class ListFilesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
