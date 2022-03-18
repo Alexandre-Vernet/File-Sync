95f9e5d38096de5ce22d36d36ea8ef687236f76d
Alexandre Vernet <alexandre.vernet@g-mail.fr>
2022-03-18 19:26:34 +0100

    Feat - Lazy loading MediaComponent

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -2,11 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SignInComponent } from './authentication/sign-in/sign-in.component';
 import { SignUpComponent } from './authentication/sign-up/sign-up.component';
-import { MediaComponent } from './media/media.component';
 import { AuthenticationGuard } from './authentication/authentication.guard';
 
 const routes: Routes = [
-    { path: '', component: MediaComponent, canActivate: [AuthenticationGuard] },
+    { path: '',  loadChildren: () => import('./media/media.module').then(m => m.MediaModule), canActivate: [AuthenticationGuard] },
     { path: 'sign-in', component: SignInComponent },
     { path: 'sign-up', component: SignUpComponent },
 ];
---
+++ front/src/app/media/media.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MediaRoutingModule } from './media-routing.module';
+import { MediaComponent } from './media.component';
+import { DialogUpdateMedia, ListMediasComponent } from './list-medias/list-medias.component';
+import { UploadMediaComponent } from './upload-media/upload-media.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
+
+@NgModule({
+    declarations: [
+        MediaComponent,
+        ListMediasComponent,
+        UploadMediaComponent,
+        DialogUpdateMedia
+    ],
+    imports: [
+        CommonModule,
+        MediaRoutingModule,
+        MatSnackBarModule,
+        MatIconModule,
+        MatButtonModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatTabsModule,
+        MatCardModule,
+        MatDialogModule,
+        FormsModule,
+        ReactiveFormsModule,
+    ],
+})
+export class MediaModule {
+}
