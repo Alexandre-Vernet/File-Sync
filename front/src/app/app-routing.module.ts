28fee49e5470fcf418fca3ebe29f9692a2b5cfa7
Alexandre Vernet <alexandre.vernet@g-mail.fr>
2022-03-18 19:09:42 +0100

    Feat - Create guard Authentication

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -3,9 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { SignInComponent } from './authentication/sign-in/sign-in.component';
 import { SignUpComponent } from './authentication/sign-up/sign-up.component';
 import { MediaComponent } from './media/media.component';
+import { AuthenticationGuard } from './authentication/authentication.guard';
 
 const routes: Routes = [
-    { path: '', component: MediaComponent },
+    { path: '', component: MediaComponent, canActivate: [AuthenticationGuard] },
     { path: 'sign-in', component: SignInComponent },
     { path: 'sign-up', component: SignUpComponent },
 ];
---
+++ front/src/app/authentication/authentication.guard.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthenticationService } from './authentication.service';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class AuthenticationGuard implements CanActivate {
+    constructor(
+        private auth: AuthenticationService,
+        private router: Router
+    ) {
+    }
+
+    canActivate(
+        route: ActivatedRouteSnapshot,
+        state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+        return new Promise((resolve) => {
+            this.auth.getAuth().then(async (user) => {
+                if (user) {
+                    resolve(true);
+                } else {
+                    this.router.createUrlTree(['/sign-in']);
+                    resolve(false);
+                }
+            });
+        });
+    }
+
+}
