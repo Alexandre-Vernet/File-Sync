dec47dc89a19333c92305bfbd4d7922eec83e50c
Alexandre Vernet <alexandre.vernet@g-mail.fr>
2022-03-31 20:44:56 +0200

    Feat - Add input material sign-in

--- front/src/app/authentication/sign-in/sign-in.module.ts
+++ front/src/app/authentication/sign-in/sign-in.module.ts
@@ -4,6 +4,9 @@ import { CommonModule } from '@angular/common';
 import { SignInRoutingModule } from './sign-in-routing.module';
 import { SignInComponent } from './sign-in.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 
 @NgModule({
@@ -13,6 +16,9 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
         SignInRoutingModule,
         FormsModule,
         ReactiveFormsModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatButtonModule,
     ]
 })
 export class SignInModule {
