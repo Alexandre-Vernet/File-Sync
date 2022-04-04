3e379b63ed62d2b3deffddaa6c1758dd0e6b9862
Alexandre Vernet <alexandre.vernet@g-mail.fr>
2022-04-04 21:09:27 +0200

    Fix - private replace \n private_key_firebase

--- api/router/file.router.js
+++ api/router/file.router.js
@@ -9,7 +9,7 @@ const admin = require("firebase-admin");
 admin.initializeApp({
     credential: admin.credential.cert({
         "project_id": process.env.FIREBASE_PROJECT_ID,
-        "private_key": process.env.FIREBASE_PRIVATE_KEY,
+        "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
         "client_email": process.env.FIREBASE_CLIENT_EMAIL
     })
 });
