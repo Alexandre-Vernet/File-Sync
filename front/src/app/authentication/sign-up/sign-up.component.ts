40931852518ca4cab1ef1af3e5465a5f1ce74698
Alexandre Vernet <alexandre.vernet@g-mail.fr>
2022-04-17 22:00:54 +0200

    Clean - Change fullName for displayName

--- front/src/app/authentication/sign-up/sign-up.component.ts
+++ front/src/app/authentication/sign-up/sign-up.component.ts
@@ -12,7 +12,7 @@ import { Router } from '@angular/router';
 export class SignUpComponent {
 
     formSignUp = new FormGroup({
-        fullName: new FormControl('', [Validators.required, Validators.minLength(5), Validators.maxLength(25)]),
+        displayName: new FormControl('', [Validators.required, Validators.minLength(5), Validators.maxLength(25)]),
         email: new FormControl('', [Validators.required, Validators.email]),
         password: new FormControl('', [Validators.required, Validators.minLength(6), Validators.maxLength(25)]),
     });
@@ -25,10 +25,10 @@ export class SignUpComponent {
 
     signUp() {
         const formValue = this.formSignUp.value;
-        const { fullName, email, password } = formValue;
+        const { displayName, email, password } = formValue;
 
         const user: UserWithPassword = {
-            displayName: fullName,
+            displayName,
             email,
             password,
             photoURL: null
