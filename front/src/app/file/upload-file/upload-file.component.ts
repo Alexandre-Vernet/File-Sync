3c58d65d5178fa0829c20897c3b73f8db086ca1e
Alexandre Vernet <alexandre.vernet@g-mail.fr>
2022-04-22 16:52:30 +0200

    WIP - Add method pastFromClipboard

--- front/src/app/file/upload-file/upload-file.component.ts
+++ front/src/app/file/upload-file/upload-file.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 import { FileService } from '../file.service';
 import { FileWithoutUrl } from '../file';
+import { getDownloadURL, getStorage, ref, uploadBytes } from 'firebase/storage';
 
 @Component({
     selector: 'app-upload-file',
@@ -10,6 +11,9 @@ import { FileWithoutUrl } from '../file';
 })
 export class UploadFileComponent {
     formFile = new FormControl('', [Validators.required]);
+
+    storage = getStorage();
+
 
     constructor(
         private fileService: FileService,
@@ -46,5 +50,44 @@ export class UploadFileComponent {
         }
 
         return this.formFile.hasError('empty') ? 'You must enter a value' : '';
+    }
+
+    async pastFromClipboard(e: any) {
+        // Get files from clipboard
+        const dataTransfer = e.clipboardData;
+        const file = dataTransfer.files[0];
+
+        const { name, type } = file;
+        const date = new Date();
+
+        const fileWithoutUrl: FileWithoutUrl = {
+            name,
+            type,
+            date
+        };
+
+        console.log(fileWithoutUrl);
+
+        // Set file target in firebase storage
+        // const fileSource = `files/${ newFile.name }`;
+        // const storageRef = ref(this.storage, fileSource);
+        //
+        // // Upload file to firebase storage
+        // uploadBytes(storageRef, newFile).then(() => {
+        //     getDownloadURL(ref(this.storage, fileSource))
+        //         .then(async (url) => {
+        //             // Get url
+        //             newFile.url = url;
+        //
+        //             // Store file in firestore
+        //             this.fileService.uploadFileStorage(newFile).subscribe((res) => {
+        //                 // Display success message
+        //                 this.fileService.displaySuccessMessage(res.message);
+        //
+        //                 // Update file list
+        //                 this.fileService.updateFileSubject();
+        //             });
+        //         });
+        // });
     }
 }
