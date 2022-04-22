598d38efb9ad76b9678faf33fa43cf0911612fd6
Alexandre Vernet <alexandre.vernet@g-mail.fr>
2022-04-22 22:05:44 +0200

    Feat - Upload file from clipboard

--- front/src/app/file/upload-file/upload-file.component.ts
+++ front/src/app/file/upload-file/upload-file.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 import { FileService } from '../file.service';
-import { FileWithoutUrl } from '../file';
+import { File, FileWithoutUrl } from '../file';
 import { getDownloadURL, getStorage, ref, uploadBytes } from 'firebase/storage';
 
 @Component({
@@ -53,41 +53,51 @@ export class UploadFileComponent {
     }
 
     async pastFromClipboard(e: any) {
-        // Get files from clipboard
-        const dataTransfer = e.clipboardData;
-        const file = dataTransfer.files[0];
+        // Get file from clipboard
+        const items = (e.clipboardData || e.originalEvent.clipboardData).items;
+        for (let index in items) {
+            const item = items[index];
+            if (item.kind === 'file') {
 
-        const { name, type } = file;
-        const date = new Date();
+                // fileSubject length
+                let length;
+                this.fileService.filesSubject.subscribe((data) => {
+                    if (data) {
+                        length = data.length;
+                    } else {
+                        length = 0;
+                    }
+                });
 
-        const fileWithoutUrl: FileWithoutUrl = {
-            name,
-            type,
-            date
-        };
+                const file: File = {
+                    name: `img - ${ length + 1 }`,
+                    url: '',
+                    type: item.type,
+                    date: new Date()
+                };
 
-        console.log(fileWithoutUrl);
+                // Set file target in firebase storage
+                const fileSource = `files/img - ${ length + 1 }`;
+                const storageRef = ref(this.storage, fileSource);
 
-        // Set file target in firebase storage
-        // const fileSource = `files/${ newFile.name }`;
-        // const storageRef = ref(this.storage, fileSource);
-        //
-        // // Upload file to firebase storage
-        // uploadBytes(storageRef, newFile).then(() => {
-        //     getDownloadURL(ref(this.storage, fileSource))
-        //         .then(async (url) => {
-        //             // Get url
-        //             newFile.url = url;
-        //
-        //             // Store file in firestore
-        //             this.fileService.uploadFileStorage(newFile).subscribe((res) => {
-        //                 // Display success message
-        //                 this.fileService.displaySuccessMessage(res.message);
-        //
-        //                 // Update file list
-        //                 this.fileService.updateFileSubject();
-        //             });
-        //         });
-        // });
+                // Upload file to firebase storage
+                uploadBytes(storageRef, item.getAsFile()).then(() => {
+                    getDownloadURL(ref(this.storage, fileSource))
+                        .then(async (url) => {
+                            // Set URL
+                            file.url = url;
+
+                            // Store file in firestore
+                            this.fileService.uploadFileStorage(file).subscribe((res) => {
+                                // Display success message
+                                this.fileService.displaySuccessMessage(res.message);
+
+                                // Update file list
+                                this.fileService.updateFileSubject();
+                            });
+                        });
+                });
+            }
+        }
     }
 }
