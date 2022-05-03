0a2342c51e5dad516afdb0e91a7f5491ffc88ab2
Alexandre Vernet <alexandre.vernet@g-mail.fr>
2022-05-03 19:39:12 +0200

    Feat - Redirect to authentication if token has expired

--- front/src/app/http.interceptor.ts
+++ front/src/app/http.interceptor.ts
@@ -3,19 +3,24 @@ import { HttpEvent, HttpHandler, HttpRequest, } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { SnackbarService } from './public/snackbar/snackbar.service';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class HttpInterceptor implements HttpInterceptor {
 
     constructor(
-        private snackbar: SnackbarService
+        private snackbar: SnackbarService,
+        private router: Router
     ) {
     }
 
     intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
         return next.handle(request)
             .pipe(
-                catchError(err => {
+                catchError((err) => {
+                    if (err.status === 401) {
+                        this.router.navigate(['/authentication']);
+                    }
                     this.snackbar.displayErrorMessage(err.error.message);
                     throw err;
                 })
