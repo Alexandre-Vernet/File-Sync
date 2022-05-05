43b24f79e5fc390750b59bd908a5203530bcde10
Alexandre Vernet <alexandre.vernet@g-mail.fr>
2022-05-05 14:58:41 +0200

    Clean - Replace 'updateMenuClassActive' with 'navigateTo'

--- front/src/app/public/navbar/navbar.component.ts
+++ front/src/app/public/navbar/navbar.component.ts
@@ -20,32 +20,35 @@ export class NavbarComponent implements OnInit {
     ) {
     }
 
-    ngOnInit(): void {
+    async ngOnInit(): Promise<void> {
         // Get current user
-        this.getUser().then((user) => {
-            this.user = user;
-        });
+        this.user = await this.auth.getAuth();
 
-        // Get current URI
-        this.currentRoute = window.location.pathname;
+        this.getCurrentRoute();
     }
 
     getUser(): Promise<User> {
         return this.auth.getAuth();
     }
 
+    getCurrentRoute(): void {
+        this.currentRoute = window.location.pathname;
+    }
+
     highlightRoute(route: string): boolean {
         return this.currentRoute === route;
     }
 
-    updateMenuClassActive(route: string) {
-        this.currentRoute = route;
+    async navigateTo(route: string): Promise<void> {
+        await this.router.navigate([route]);
+        this.getCurrentRoute();
     }
 
     signOut() {
         this.auth.signOut().then(async () => {
             this.auth.user = null;
             localStorage.removeItem('token');
+            localStorage.removeItem('customToken');
             await this.router.navigateByUrl('/authentication');
         });
     }
