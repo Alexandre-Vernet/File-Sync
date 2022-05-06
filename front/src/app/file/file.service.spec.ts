04effb7e0b2c3008a3ff9db55857bc4228b39af1
Alexandre Vernet <alexandre.vernet@g-mail.fr>
2022-05-06 11:51:21 +0200

    Test - FileService

--- front/src/app/file/file.service.spec.ts
+++ front/src/app/file/file.service.spec.ts
@@ -1,16 +1,25 @@
 import { TestBed } from '@angular/core/testing';
 
 import { FileService } from './file.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 describe('FileService', () => {
-  let service: FileService;
+    let service: FileService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(FileService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [
+                HttpClientTestingModule,
+                RouterTestingModule,
+                MatSnackBarModule
+            ],
+        });
+        service = TestBed.inject(FileService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
 });
