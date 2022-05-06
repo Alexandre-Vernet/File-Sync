2f41930dab612c8a4681fadb442b2b90a70f4d40
Alexandre Vernet <alexandre.vernet@g-mail.fr>
2022-05-06 20:24:31 +0200

    Fix - import moment

--- front/src/app/file/file.service.spec.ts
+++ front/src/app/file/file.service.spec.ts
@@ -4,11 +4,20 @@ import { FileService } from './file.service';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { UserWithId } from '../authentication/user';
+import { FileWithId } from './file';
+import { AuthenticationService } from '../authentication/authentication.service';
 
 describe('FileService', () => {
     let service: FileService;
+    let user: UserWithId = {
+        displayName: 'Alexandre Vernet',
+        email: 'alexandre.vernet@g-mail.fr',
+        uid: 'OEe3li0OEhNxvECIweMfD0flvA63'
+    };
 
     beforeEach(() => {
+        jest.setTimeout(30000);
         TestBed.configureTestingModule({
             imports: [
                 HttpClientTestingModule,
@@ -16,10 +25,37 @@ describe('FileService', () => {
                 MatSnackBarModule
             ],
         });
+
+        // Mock the authentication service
+        let auth: AuthenticationService;
+        auth = TestBed.inject(AuthenticationService);
+        auth.user = user;
         service = TestBed.inject(FileService);
     });
 
     it('should be created', () => {
         expect(service).toBeTruthy();
+    });
+
+    describe('getFiles', () => {
+        it('should return an array of files', async (done) => {
+
+            const result: FileWithId = {
+                url: 'https://firebasestorage.googleapis.com/v0/b/media-share-4f34a.appspot.com/o/files%2Fwp2818490.jpg?alt=media&token=4218fbb8-262b-45bd-848e-16bad5f91582',
+                date: new Date('2022-05-06T15:39:10.405Z'),
+                id: 'ndynx4p9amrw808oktqmdm',
+                name: 'wp2818490.jpg',
+                size: 348578,
+                type: 'image/jpeg'
+            };
+
+            const files = await service.getFiles(user.uid);
+            files.subscribe((data: FileWithId[]) => {
+                console.log(data);
+                done();
+            }, (error) => {
+                console.log(error);
+            });
+        });
     });
 });
