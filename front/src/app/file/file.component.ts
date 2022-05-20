2dae7c754f17e43cb999ae3d9cdadac6557ca399
Alex <alexandre.vernet@g-mail.fr>
2022-05-20 20:33:31 +0200

    Fix - Subscribe to notification

--- front/src/app/file/file.component.ts
+++ front/src/app/file/file.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NotificationService } from '../notification/notification.service';
 
 @Component({
     selector: 'app-file',
     templateUrl: './file.component.html',
     styleUrls: ['./file.component.scss']
 })
-export class FileComponent {
+export class FileComponent implements OnInit {
 
-    constructor() {
+    constructor(
+        private notification: NotificationService
+    ) {
+    }
+
+    ngOnInit() {
+        this.notification.subscribeNotification();
     }
 }
