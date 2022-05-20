9ea813b398714d1508a76c621841fa72b40f1e41
Alex <alexandre.vernet@g-mail.fr>
2022-05-20 20:43:26 +0200

    Feat - Create guard verify-email

---
+++ front/src/app/authentication/verify-email.guard.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class VerifyEmailGuard implements CanActivate {
+    constructor(
+        private http: HttpClient
+    ) {
+    }
+
+    canActivate(
+        route: ActivatedRouteSnapshot,
+        state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+
+
+        return true;
+    }
+
+}
