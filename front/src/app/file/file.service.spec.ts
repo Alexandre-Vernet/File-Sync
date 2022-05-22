584b1be970d9b3b9dd05f82a0bf6b5caf98001c6
Alex <alexandre.vernet@g-mail.fr>
2022-05-22 21:16:05 +0200

    Fix - test

--- front/src/app/file/file.service.spec.ts
+++ front/src/app/file/file.service.spec.ts
@@ -5,7 +5,6 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { UserWithId } from '../authentication/user';
-import { FileWithId } from './file';
 import { AuthenticationService } from '../authentication/authentication.service';
 
 describe('FileService', () => {
@@ -13,7 +12,8 @@ describe('FileService', () => {
     let user: UserWithId = {
         displayName: 'Alexandre Vernet',
         email: 'alexandre.vernet@g-mail.fr',
-        uid: 'OEe3li0OEhNxvECIweMfD0flvA63'
+        uid: 'OEe3li0OEhNxvECIweMfD0flvA63',
+        emailVerified: true
     };
 
     beforeEach((done) => {
@@ -41,15 +41,6 @@ describe('FileService', () => {
 
     describe('getFiles', () => {
         it('should return an array of files', (done) => {
-
-            const mock: FileWithId = {
-                url: 'https://firebasestorage.googleapis.com/v0/b/media-share-4f34a.appspot.com/o/files%2Fwp2818490.jpg?alt=media&token=4218fbb8-262b-45bd-848e-16bad5f91582',
-                date: new Date('2022-05-06T15:39:10.405Z'),
-                id: 'ndynx4p9amrw808oktqmdm',
-                name: 'wp2818490.jpg',
-                size: 348578,
-                type: 'image/jpeg'
-            };
 
             service.getFiles(user.uid).subscribe(files => {
                 console.log(files);
