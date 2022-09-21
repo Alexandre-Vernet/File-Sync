e2ddfc66a79b36c4b2a18e52a2958de1cf7f9fed
Alex <alexandre.vernet@g-mail.fr>
2022-09-21 20:02:49 +0200

    Feat - Display totalFilesSize

--- front/src/app/file/storage-usage/storage-usage.component.ts
+++ front/src/app/file/storage-usage/storage-usage.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { File } from '../file';
+import { FileService } from '../file.service';
 
 @Component({
-  selector: 'app-storage-usage',
-  templateUrl: './storage-usage.component.html',
-  styleUrls: ['./storage-usage.component.scss']
+    selector: 'app-storage-usage',
+    templateUrl: './storage-usage.component.html',
+    styleUrls: ['./storage-usage.component.scss']
 })
 export class StorageUsageComponent implements OnInit {
+    storageUsage = {
+        totalFilesSize: '0',
+        progressBarValue: 0
+    };
 
-  constructor() { }
+    constructor(
+        private fileService: FileService
+    ) {
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        this.fileService.filesSubject.subscribe((files) => {
+            if (files) {
+                const totalSize = File.getTotalSize(files);
+
+                // Convert files size in percentage (5GB = 100%)
+                this.storageUsage.progressBarValue = Math.round(totalSize / 5000000000 * 100);
+
+                // Display total files size
+                this.storageUsage.totalFilesSize = File.convertSize(totalSize);
+            }
+        });
+    }
 
 }
