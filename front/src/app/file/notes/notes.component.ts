5bb53ef9a05353cdee7c5dca8bc794d185cc47b5
Alex <alexandre.vernet99@gmail.com>
2022-10-27 15:17:43 +0200

    Feat - Display markdown code in new ng-container

--- front/src/app/file/notes/notes.component.ts
+++ front/src/app/file/notes/notes.component.ts
@@ -24,7 +24,7 @@ export class NotesComponent {
 
     uploadNote() {
         const name = this.formFile.value;
-        const type = 'text/plain';
+        const type = name.includes('#') ? 'text/markdown' : 'text/plain';
         const size = 0;
         const date = new Date();
 
