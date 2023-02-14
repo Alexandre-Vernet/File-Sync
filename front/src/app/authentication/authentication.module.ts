15e2cbba9548aad22cd05cd1d6efb9531b90d872
Alex <alexandre.vernet@g-mail.fr>
2023-02-14 21:23:37 +0100

    feat(verify email): Create a modal to verify email

--- front/src/app/authentication/authentication.module.ts
+++ front/src/app/authentication/authentication.module.ts
@@ -10,13 +10,13 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import {
     DialogDeleteAccountComponent,
-    UserProfileComponent,
-    DialogDeleteFilesComponent
+    DialogDeleteFilesComponent,
+    UserProfileComponent
 } from './user-profile/user-profile.component';
 import { AuthenticationComponent } from './authentication.component';
 import { NavbarModule } from '../public/navbar/navbar.module';
 import { MatDialogModule } from '@angular/material/dialog';
-import { VerifyEmailComponent } from './verify-email/verify-email.component';
+import { DialogVerifyEmailComponent, VerifyEmailComponent } from './verify-email/verify-email.component';
 import { AuthenticationPipe } from './authentication.pipe';
 
 
@@ -30,6 +30,7 @@ import { AuthenticationPipe } from './authentication.pipe';
         DialogDeleteFilesComponent,
         VerifyEmailComponent,
         DialogResetPasswordFileComponent,
+        DialogVerifyEmailComponent,
         AuthenticationPipe
     ],
     imports: [
--- front/src/app/authentication/verify-email/verify-email.component.ts
+++ front/src/app/authentication/verify-email/verify-email.component.ts
@@ -2,23 +2,28 @@ import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '../authentication.service';
 import { Router } from '@angular/router';
 import { SnackbarService } from '../../public/snackbar/snackbar.service';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
-    selector: 'app-verify-email',
-    templateUrl: './verify-email.component.html',
-    styleUrls: ['./verify-email.component.scss']
+    template: `
+        <h2 mat-dialog-title>Verify email</h2>
+        <mat-dialog-content class="mat-typography">
+            <h3> Please, check your email for the activation link.</h3>
+            <p>Then, press refresh button</p>
+        </mat-dialog-content>
+        <mat-dialog-actions class="ion-justify-content-end">
+            <button mat-button (click)="redirectToSignIn()" [mat-dialog-close]="true">Cancel</button>
+            <button mat-button (click)="checkIfEmailIsVerified()" cdkFocusInitial>Refresh</button>
+        </mat-dialog-actions>
+    `,
 })
-export class VerifyEmailComponent implements OnInit {
+export class DialogVerifyEmailComponent {
 
     constructor(
         private auth: AuthenticationService,
         private router: Router,
         private snackbar: SnackbarService,
     ) {
-    }
-
-    async ngOnInit() {
-        await this.checkIfEmailIsVerified();
     }
 
     async checkIfEmailIsVerified() {
@@ -47,5 +52,30 @@ export class VerifyEmailComponent implements OnInit {
                     await this.router.navigateByUrl('/authentication/sign-in');
                 }
             });
+    }
+
+    async redirectToSignIn() {
+        await this.router.navigateByUrl('/authentication/sign-in');
+    }
+}
+
+@Component({
+    selector: 'app-verify-email',
+    templateUrl: './verify-email.component.html',
+    styleUrls: ['./verify-email.component.scss']
+})
+export class VerifyEmailComponent implements OnInit {
+
+    constructor(
+        public dialog: MatDialog
+    ) {
+    }
+
+    ngOnInit() {
+        this.openDialogVerifyEmail();
+    }
+
+    openDialogVerifyEmail() {
+        this.dialog.open(DialogVerifyEmailComponent);
     }
 }
