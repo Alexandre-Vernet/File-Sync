950fd36d0e4796d0cd2d932a5a66610afaeda7c8
Alexandre Vernet <alexandre.vernet@g-mail.fr>
2023-03-11 20:37:48 +0100

    feat(sign-in): Display error from the catch if getCustomErrorMessage return ''

--- front/src/app/authentication/sign-in/sign-in.component.ts
+++ front/src/app/authentication/sign-in/sign-in.component.ts
@@ -13,8 +13,8 @@ import { AuthenticationPipe } from '../authentication.pipe';
 export class SignInComponent {
 
     formSignIn = new FormGroup({
-        email: new FormControl('', [Validators.required, Validators.email]),
-        password: new FormControl('', [Validators.required, Validators.minLength(6)])
+        email: new FormControl('alexandre.vernet99@gmail.com', [Validators.required, Validators.email]),
+        password: new FormControl('alexandre', [Validators.required, Validators.minLength(6)])
     });
 
     constructor(
@@ -34,6 +34,11 @@ export class SignInComponent {
             })
             .catch((error) => {
                 const errorMsg = new AuthenticationPipe().getCustomErrorMessage(error.code);
+                if (errorMsg === '') {
+                    this.formSignIn.controls.email.setErrors({
+                        'auth': error
+                    });
+                }
                 this.formSignIn.controls.email.setErrors({
                     'auth': errorMsg
                 });
