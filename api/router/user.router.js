6a0afc614ca73c3da9d41df0d28ea8277008c103
Alexandre Vernet <alexandre.vernet@g-mail.fr>
2023-03-11 20:46:16 +0100

    feat(sign-in): Add log

--- api/router/user.router.js
+++ api/router/user.router.js
@@ -42,10 +42,12 @@ users.get('/:uid', async (req, res) => {
                     res.status(200).send({ token })
                 })
                 .catch((error) => {
+                    console.log(error)
                     res.status(500).send({ error })
                 });
         })
         .catch((error) => {
+            console.log(error)
             res.status(500).send({ error });
         });
 });
