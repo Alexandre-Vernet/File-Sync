4e294aa64cbb9cd3c6d89b21d5a9e1ea52360706
Alexandre Vernet <alexandre.vernet@g-mail.fr>
2023-03-21 16:57:55 +0100

    feat: Detect code

--- front/src/app/file/file.pipe.ts
+++ front/src/app/file/file.pipe.ts
@@ -28,7 +28,7 @@ export class FilePipe implements PipeTransform {
         }
     }
 
-    determineFileType(fileName: string, type: string) {
+    determineFileType(fileName: string, type: string): string {
         if (fileName.includes('.rar') || fileName.includes('.zip')) {
             return 'application/zip';
         }
@@ -47,9 +47,19 @@ export class FilePipe implements PipeTransform {
         // underscores for italicized text, backticks for code text, brackets for links and images, and hashtags for headings.
         const markdownRegex = /(\*\*|_|`|#+|\[.\]|\n- .*)/gm;
         // The test method of the regex object returns true if any of the special characters are detected in the input text.
-        return markdownRegex.test(text);
+        return markdownRegex.test(text) || this.detectCode(text);
     }
 
+    detectCode(text: string): boolean {
+        const codeRegex = /(<[a-z][\s\S]*>|<\s*[a-z][\s\S]*\/>|<\s*[a-z][\s\S]*>[\s\S]*<\/[a-z]+>|<\s*[a-z][\s\S]*>)/i;
+        const cssRegex = /(\{[\s\S]*\})/i;
+        const jsRegex = /(function[\s\S]*\()|(class[\s\S]*\()|(const[\s\S]*\()|(let[\s\S]*\()|(var[\s\S]*\()|(if[\s\S]*\()|(else[\s\S]*\()|(switch[\s\S]*\()|(case[\s\S]*\()|(break[\s\S]*\()|(for[\s\S]*\()|(while[\s\S]*\()|(do[\s\S]*\()|(return[\s\S]*\()|(console[\s\S]*\()|(alert[\s\S]*\()|(document[\s\S]*\()|(window[\s\S]*\()|(addEventListener[\s\S]*\()|(setTimeout[\s\S]*\()|(setInterval[\s\S]*\()|(clearInterval[\s\S]*\()|(clearTimeout[\s\S]*\()|(fetch[\s\S]*\()|(XMLHttpRequest[\s\S]*\()|(axios[\s\S]*\()|(async[\s\S]*\()|(await[\s\S]*\()|(try[\s\S]*\()|(catch[\s\S]*\()|(throw[\s\S]*\()|(typeof[\s\S]*\()|(instanceof[\s\S]*\()|(import[\s\S]*\()|(export[\s\S]*\()/i;
+        const phpRegex = /(<\?php[\s\S]*\?>)|(<\?[\s\S]*\?>)/i;
+        const cppRegex = /(cout[\s\S]*<<[\s\S]*;)|(cin[\s\S]*>>[\s\S]*;)|(for[\s\S]*\()|(while[\s\S]*\()|(do[\s\S]*\()|(if[\s\S]*\()|(else[\s\S]*\()|(switch[\s\S]*\()|(case[\s\S]*\()|(break[\s\S]*\()|(return[\s\S]*\()|(auto[\s\S]*[\s\S]*;)|(int[\s\S]*[\s\S]*;)|(double[\s\S]*[\s\S]*;)|(float[\s\S]*[\s\S]*;)|(char[\s\S]*[\s\S]*;)|(bool[\s\S]*[\s\S]*;)|(string[\s\S]*[\s\S]*;)/i;
+        const pythonRegex = /^(\s*import\s+\w+|\s*from\s+\w+\s+import\s+\w+|\s*def\s+\w+\(.*\):\s*|\s*class\s+\w+.*:\s*|\s*if\s+\w+.*:\s*|\s*for\s+\w+.*:\s*|\s*while\s+\w+.*:\s*|\s*try:\s*|\s*except\s+\w+.*:\s*|\s*finally:\s*|\s*with\s+\w+.*:\s*|\s*async\s+def\s+\w+\(.*\):\s*)$/i;
+        return codeRegex.test(text) || cssRegex.test(text) || jsRegex.test(text) || phpRegex.test(text) || cppRegex.test(text) || pythonRegex.test(text);
+    }
+    
     getTotalSize(files: FileWithId[]): number {
         let totalFilesSize: number = 0;
         files.forEach((file) => {
