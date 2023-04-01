bd3ccb6e9697c247d017f984d1b0c5a76f2c6d8c
Alexandre Vernet <alexandre.vernet99@gmail.com>
2023-04-02 01:53:00 +0200

    test(cors): Add headers

--- api/router/app.router.js
+++ api/router/app.router.js
@@ -2,14 +2,8 @@ const express = require('express');
 const router = express.Router();
 const fileRouter = require('./file.router');
 const userRouter = require('./user.router');
-const cors = require('cors');
 const { verifyAccessToken } = require("../middlewares/jwt");
 
-const frontendUrl = process.env.FRONTEND_URL || 'http://localhost:4200';
-
-router.use(cors({
-    origin: frontendUrl
-}));
 
 router.use('/files', verifyAccessToken, ((req, res, next) => {
     next();
