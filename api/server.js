93a0e665696a9477f76867de00bfa44db4faa4e5
Alexandre Vernet <alexandre.vernet99@gmail.com>
2023-04-01 22:08:13 +0200

    feat: Refacto architecture

--- api/server.js
+++ api/server.js
@@ -6,13 +6,6 @@ const router = require('./router/app.router');
 const validateEnvVariables = require("./config/validateEnvVariables");
 
 app.use(express.json());
-app.use(express.static('public'));
-
-
-app.get('/', async (req, res) => {
-    res.sendFile(`${ __dirname }/public/index.html`);
-});
-
 
 app.use('/api', (req, res, next) => {
     next();
