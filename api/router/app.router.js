d63e517371c8c8a8787d553c865b46061e0a646d
Alexandre Vernet <alexandre.vernet99@gmail.com>
2023-04-02 01:54:51 +0200

    Revert "test(cors): Add headers"
    This reverts commit bd3ccb6e9697c247d017f984d1b0c5a76f2c6d8c.

--- api/router/app.router.js
+++ api/router/app.router.js
@@ -2,8 +2,14 @@ const express = require('express');
 const router = express.Router();
 const fileRouter = require('./file.router');
 const userRouter = require('./user.router');
+const cors = require('cors');
 const { verifyAccessToken } = require("../middlewares/jwt");
 
+const frontendUrl = process.env.FRONTEND_URL || 'http://localhost:4200';
+
+router.use(cors({
+    origin: frontendUrl
+}));
 
 router.use('/files', verifyAccessToken, ((req, res, next) => {
     next();
--- api/server.js
+++ api/server.js
@@ -8,11 +8,6 @@ const validateEnvVariables = require("./config/validateEnvVariables");
 app.use(express.json());
 
 app.use('/api', (req, res, next) => {
-    const frontendUrl = process.env.FRONTEND_URL || 'http://localhost:4200';
-    res.setHeader('Access-Control-Allow-Origin', frontendUrl);
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Credentials', true);
     next();
 }, router);
 
