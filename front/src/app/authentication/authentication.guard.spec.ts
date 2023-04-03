619c1697ea42b72b733e7b8f6ab700b6c592e680
Alexandre Vernet <alexandre.vernet99@gmail.com>
2023-04-03 20:55:45 +0200

    clean(test file): Remove call to initializeApp

--- front/src/app/authentication/authentication.guard.spec.ts
+++ front/src/app/authentication/authentication.guard.spec.ts
@@ -1,26 +1,22 @@
 import { TestBed } from '@angular/core/testing';
-
 import { AuthenticationGuard } from './authentication.guard';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { initializeApp } from 'firebase/app';
-import { firebaseConfig } from '../../environments/environment';
 
 describe('AuthenticationGuard', () => {
-  let guard: AuthenticationGuard;
+    let guard: AuthenticationGuard;
 
-  beforeEach(() => {
-    initializeApp(firebaseConfig);
+    beforeEach(() => {
 
-    TestBed.configureTestingModule({
-      providers: [AuthenticationGuard],
-      imports: [HttpClientTestingModule, RouterTestingModule, MatSnackBarModule],
+        TestBed.configureTestingModule({
+            providers: [AuthenticationGuard],
+            imports: [HttpClientTestingModule, RouterTestingModule, MatSnackBarModule],
+        });
+        guard = TestBed.inject(AuthenticationGuard);
     });
-    guard = TestBed.inject(AuthenticationGuard);
-  });
 
-  it('should be created', () => {
-    expect(guard).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(guard).toBeTruthy();
+    });
 });
