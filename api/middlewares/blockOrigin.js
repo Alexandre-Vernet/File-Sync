5deee69d5e947986347149e04976385377364fcf
Alex <alexandre.vernet99@gmail.com>
2023-04-05 12:15:08 +0200

    feat(CORS): Create middleware blockOrigin

---
+++ api/middlewares/blockOrigin.js
@@ -0,0 +1,18 @@
+const blockOrigin = (req, res, next) => {
+    const { NODE_ENV, FRONTEND_URL } = process.env;
+    const origin = req.get('origin');
+    console.log('origin: ' + origin)
+
+    if (FRONTEND_URL === origin) {
+        next();
+    } else {
+        if (NODE_ENV === 'development') {
+            next();
+            return;
+        }
+        console.error('Forbidden request from: ' + origin);
+        res.status(403).send('Forbidden');
+    }
+}
+
+module.exports = { blockOrigin };
--- api/server.js
+++ api/server.js
@@ -4,24 +4,11 @@ const port = process.env.PORT || 3000;
 require('dotenv').config();
 const router = require('./router/app.router');
 const validateEnvVariables = require("./config/validateEnvVariables");
+const { blockOrigin } = require("./middlewares/blockOrigin");
 
 app.use(express.json());
 
-app.use('/api', (req, res, next) => {
-    const { NODE_ENV, FRONTEND_URL } = process.env;
-    const origin = req.get('origin');
-
-    if (FRONTEND_URL === origin) {
-        next();
-    } else {
-        if (NODE_ENV === 'development') {
-            next();
-            return;
-        }
-        console.error('Forbidden request from: ' + origin);
-        res.status(403).send('Forbidden');
-    }
-}, router);
+app.use('/api', blockOrigin, router);
 
 validateEnvVariables();
 
