9a2abeb378cf59a705f711298fd05d5a13ac352c
Alexandre Vernet <alexandre.vernet99@gmail.com>
2024-01-20 18:58:45 +0100

    feat(docker): Add ports in .env

--- api/server.js
+++ api/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 4002;
+const port = process.env.PORT;
 require('dotenv').config();
 const router = require('./router/router');
 const validateEnvVariables = require("./config/validateEnvVariables");
